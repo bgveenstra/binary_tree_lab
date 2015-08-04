59edb39a6c6c8ee2508f9aa69ad31c685351960e
Delmer Reed <delmer814@yahoo.com>
2015-08-04 09:15:27 -0700

    first

---
+++ lib/binary_tree.rb
@@ -0,0 +1,119 @@
+##
+# This class represents a *sorted*
+# tree with _exactly_ _two_ _subtrees_.
+# Follow the link for more info on
+# {Sorted Binary Search Trees}[https://en.wikipedia.org/wiki/Binary_search_tree]
+
+class BinaryTree
+
+
+  attr_accessor :value, :left, :right
+
+  ##
+  # :attr_accessor: value
+  # read or write the value of the tree
+  #
+  #  my_tree = BinaryTree.new()
+  #  my_tree.value = 5
+  #  #=> 5
+  #  my_tree.value
+  #  #=> 5
+
+  ##
+  # :attr_accessor: right
+  # initializes and reads the +right+ subtree for the instance
+  #
+  #   my_tree = BinaryTree.new()
+  #   #=> #<BinaryTree:0x007ff8a412a710 ... >
+  #
+  #   my_tree.right
+  #   #=> nil
+  #
+  #   my_tree.right = BinaryTree.new()
+  #   #=> #<BinaryTree:0x007ff73e00e690 ...>
+  #
+  #   my_tree.right
+  #   #=> #<BinaryTree:0x007ff73e00e690 ...>
+
+  ##
+  # :attr_accessor: left
+  # initializes and reads +left+ subtree 
+  # for an instance. See #right for usage.
+
+  ##
+  # Creates a new binary tree 
+  # with the optional +val+ param 
+  # as the @value for the instance
+  #
+  #  my_tree = BinaryTree.new
+  #  #=> #<BinaryTree:0x007ff8a4073790 @value=nil>
+  #
+  # or pass in an initial value
+  #
+  #  my_tree = BinaryTree.new(8)
+  #  #=> #<BinaryTree:0x007ff8a412a710 @value=8> 
+  #  my_tree.value
+  #  #=> 8
+
+  def initialize(val=nil)
+    @value = val
+  end
+
+  ##
+  # helper method to check if both 
+  # +@left+ and +@right+ subtrees are +nil+
+
+  def leaf?
+
+  end
+
+  ##
+  # will determine where to place a
+  # new +val+: @value, @left, or @right. 
+  #
+  # If the instance already has a +#value+ then a 
+  # new value will be placed in either the +#left+
+  # or +#right+ subtrees if new +val+ is
+  # less than or greater than +#value+ respectively.
+  #
+  #   my_tree = BinaryTree.new(5)
+  #   #=> #<BinaryTree:0x007ff73e014f90 @value=5>
+  #   
+  #   # for a new val less than 5 it is inserted left
+  #   my_tree.insert(4)
+  #   #=> #<BinaryTree:0x007ff73e014f90 @value=5 @left=#<BinaryTree:0x007ff73c06a2f8 @value=4>>
+  #
+  #   # for a new +val greater than or equal to 5 it is inserted to right
+  #   my_tree.insert(6)
+  #   #=> #<BinaryTree:0x007ff73e014f90 @value=5 @left=... @right=#<BinaryTree:0x007ff73c06a2f8 @value=6>>
+
+  def insert(val)
+
+  end
+
+  ##
+  # A method to check if +val+ is in tree
+  # that returns +true+ if in the tree 
+  # and +false+ if not.
+
+  def include?(val)
+
+  end
+
+  ##
+  # Will calculate the height of 
+  # a BinaryTree by traversing 
+  # each sub-tree  
+
+  def height
+  
+  end
+
+  ##
+  # method to convert the tree to a sorted array
+
+  def to_arr
+
+  end
+
+end
---
+++ spec/binary_tree_spec.rb
@@ -0,0 +1,135 @@
+require "./lib/binary_tree.rb"
+
+describe "BinaryTree" do
+
+  describe "#attr_accessors" do
+
+    it "should have :value and :value=" do
+      tree = BinaryTree.new
+      expect(tree).to respond_to(:value)
+      expect(tree).to respond_to(:value=)
+    end
+
+
+    it "should have :left and :left=" do
+      tree = BinaryTree.new
+      expect(tree).to respond_to(:left)
+      expect(tree).to respond_to(:left=)
+    end
+
+    it "should have :right and :right=" do
+      tree = BinaryTree.new
+      expect(tree).to respond_to(:right)
+      expect(tree).to respond_to(:right=)
+    end
+
+  end
+
+  describe "#new" do 
+
+    it "should accept a value" do
+      tree = BinaryTree.new(5)
+      expect(tree.value).to eql(5)
+    end
+
+  end
+
+  describe "#insert" do
+
+    before(:each) do 
+      @tree = BinaryTree.new 
+    end
+
+    it "should update the :value if nil" do
+      @tree.insert(5)
+      expect(@tree.value).to eql(5)
+    end
+
+    it "should insert to the left for less than values" do
+      @tree.insert(5)
+      @tree.insert(4)
+      left = @tree.left
+      expect(left.value).to eql(4)
+    end
+
+    it "should insert to the right for greater than values" do
+      @tree.insert(5)
+      @tree.insert(6)
+      left = @tree.left
+      expect(left.value).to eql(6)
+    end
+
+    it "should return instance after insert" do
+      return_val = @tree.insert(4)
+      expect(return_val).to eql(@tree)
+    end
+
+  end
+
+  describe "#height" do
+
+    before(:each) do
+      @tree = BinaryTree.new(50)
+      @tree.insert(25).insert(75)
+      @tree.insert(13).insert(37)
+      @tree.insert(63).insert(87)
+    end
+
+    it "should take the max height" do
+      expect(@tree.height).to eql(2)
+    end
+
+  end
+
+  describe "#include?" do
+
+    before(:each) do
+      @tree = BinaryTree.new(50)
+      @tree.insert(25).insert(75)
+      @tree.insert(13).insert(37)
+      @tree.insert(63).insert(87)
+    end
+
+    it "should take the max height" do
+      aggregate_failures("inclusions") do
+        expect(@tree.include?(63)).to be(true)
+        expect(@tree.include?(37)).to be(true)
+        expect(@tree.include?(10)).to be(false)
+      end
+    end
+  end
+
+  describe "#leaf?" do
+
+    before(:each) do
+      @tree = BinaryTree.new(5)
+    end
+
+    it "should return true tree with no left and right trees" do
+      expect(@tree.leaf?).to be(true)
+    end
+
+    it "should return false for tree with subtree" do
+      @tree.insert(4)
+      expect(@tree.leaf?).to be(false)
+    end
+
+  end
+
+  describe "#to_arr" do
+
+    before(:each) do
+      @tree = BinaryTree.new(50)
+      @tree.insert(25).insert(75)
+      @tree.insert(13).insert(37)
+      @tree.insert(63).insert(87)
+    end
+
+    it "should be return a sorted arr" do
+      result = @tree.to_arr
+      expect(result).to eql([13, 25, 37, 50, 63, 75, 87])
+    end
+
+  end
+
+end
